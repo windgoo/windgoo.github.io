71bebd130ec0717e64019be240a1218508de0e07
windgoo <2603513186@qq.com>
2025-08-10 21:03:06 +0800

    Site updated: 2025-08-10 21:03:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/23/hello-world/"];function toRandomPost(){
+var posts=["2025/05/12/CS通过frp实现内网穿透教程及原理分析/","2025/04/23/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
