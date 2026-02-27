ed5fceef7a70acffb9d82d1feb5930e5f514d0b5
windgoo <2603513186@qq.com>
2026-02-28 01:31:00 +0800

    Site updated: 2026-02-28 01:30:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/23/hello-world/","2025/05/12/CS通过frp实现内网穿透教程及原理分析/"];function toRandomPost(){
+var posts=["2025/05/12/CS通过frp实现内网穿透教程及原理分析/","2026/02/28/手把手教你破解WiFi/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
